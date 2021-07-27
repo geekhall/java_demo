76bf253cbad12e536e04ee8ab559bff39cff47ac
yiny <174821300@qq.com>
2021-07-27 20:03:21 +0800

    20210727-200321

---
+++ mybatis-05-anno/src/main/java/cn/geekhall/dao/UserMapper.java
@@ -0,0 +1,33 @@
+package cn.geekhall.dao;
+
+import cn.geekhall.bean.User;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * UserMapper.java
+ *
+ * @author yiny
+ */
+public interface UserMapper {
+    // 获取所有用户
+    @Select("select * from user")
+    List<User> getUserList();
+
+
+    // 若方法存在多个参数，则所有的参数前都需要加上@Param
+    @Select("select * from user where id = #{id}")
+    User getUserById(@Param("id") int id);
+
+
+    @Insert("insert into user (id, name, pwd) values (#{id},#{name}, #{pwd})")
+    int insertUser(User user );
+
+    @Update("update user set name=#{name}, pwd=#{pwd} where id = #{id}")
+    int updateUser(User user);
+
+    @Delete("delete user where id= #{id}")
+    int deleteUser(int id);
+}
---
+++ mybatis-05-anno/src/test/java/cn/geekhall/dao/UserTest.java
@@ -0,0 +1,73 @@
+package cn.geekhall.dao;
+
+import cn.geekhall.bean.User;
+import cn.geekhall.util.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * UserTest.java
+ *
+ * @author yiny
+ */
+public class UserTest {
+
+    @Test
+    public void test(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> users = mapper.getUserList();
+        for (User user : users) {
+            System.out.println(user);
+        }
+        sqlSession.close();
+    }
+
+    @Test
+    public void getUserByIdTest(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user1 = mapper.getUserById(1);
+        System.out.println(user1);
+        sqlSession.close();
+    }
+
+    @Test
+    public void insertUserTest(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user1 = new User(4, "KKK", "123123");
+        int res  = mapper.insertUser(user1);
+        if (res > 0 ){
+            System.out.println("插入成功！");
+        }
+        sqlSession.close();
+    }
+
+    @Test
+    public void updateUserTest(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user1 = new User(4, "oooKKK", "123123123");
+        int res = mapper.updateUser(user1);
+        if (res > 0 ){
+            System.out.println("更新成功！");
+        }
+        sqlSession.close();
+    }
+    
+
+    @Test
+    public void deleteUserTest(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        int res  = mapper.deleteUser(4);
+        if (res > 0 ){
+            System.out.println("删除成功！");
+        }
+        sqlSession.close();
+    }
+
+}
