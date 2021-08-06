f6af382da92eebf83e11797cb58ba65db4896521
yiny <174821300@qq.com>
2021-08-06 21:11:59 +0800

    20210806-211159

---
+++ springboot-04-web/src/main/java/cn/geekhall/config/MyMvcConfig.java
@@ -0,0 +1,41 @@
+package cn.geekhall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Locale;
+
+/**
+ * MyMvcConfig.java
+ * 扩展SpringMVCS视图解析器
+ * @author yiny
+ */
+@Configuration
+//@EnableWebMvc   // 这个注解的作用就是导入了DelegatingWebMvcConfiguration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // 浏览器发送/geekhall，就会跳转到success页面。
+        registry.addViewController("/geekhall").setViewName("success");
+    }
+
+    // 实现了视图解析器接口的类，我们就可以把它看作是视图解析器。
+    public ViewResolver myViewResolver(){
+        return new MyViewResolver();
+    }
+
+    // 自定义一个视图解析器
+    public static class MyViewResolver implements ViewResolver{
+
+        @Override
+        public View resolveViewName(String s, Locale locale) throws Exception {
+            return null;
+        }
+    }
+
+}
--- springboot-04-web/src/main/java/cn/geekhall/controller/HelloController.java
+++ springboot-04-web/src/main/java/cn/geekhall/controller/HelloController.java
@@ -1,18 +1,27 @@
 package cn.geekhall.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 /**
  * HelloController.java
  *
  * @author yiny
  */
-@RestController
+@Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello spring boot";
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("msg", "GeekHall");
+        model.addAttribute("utext", "<h2 style='color:red'>GeekHall</h2>");
+        model.addAttribute("users", Arrays.asList("Geek", "Hall"));
+
+        return "hello";
     }
 }
